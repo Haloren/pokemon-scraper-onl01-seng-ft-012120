b776df204db386a73d4fe359f55d664b34d86985
Kyle McKenna <mckennakyler@gmail.com>
2020-02-20 18:08:20 +0000

    Automatically backed up by Learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,6 +1,12 @@
 class Pokemon
-
-
+  attr_accessor :name, :type
+  attr_reader :id
+  def initialize (name:, type:, id:, db:)
+    @name = name
+    @type = type
+    @id = id 
+    @db = db 
+  end   
 
 
 
