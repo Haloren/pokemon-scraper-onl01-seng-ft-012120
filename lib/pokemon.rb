b31816f771fac66994a634042acedf8446e356f0
Kyle McKenna <mckennakyler@gmail.com>
2020-02-20 18:11:54 +0000

    Automatically backed up by Learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -10,7 +10,12 @@ class Pokemon
   end   
 
   def self.save (name, type, db)
-    
+    sql = <<-SQL
+            INSERT INTO pokemon (name, type)
+            VALUES (?, ?)
+        SQL
+
+        db.execute(sql, name, type)
   end   
 
 
