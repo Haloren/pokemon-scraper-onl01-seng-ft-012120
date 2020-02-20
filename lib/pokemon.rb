c457e4606d043d983cf768f9eafb0efe0e4f3a77
Kyle McKenna <mckennakyler@gmail.com>
2020-02-20 18:30:55 +0000

    Automatically backed up by Learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -18,8 +18,16 @@ class Pokemon
     db.execute(sql, name, type)
   end   
 
-  def self.find 
-    
+  def self.find(id, db)
+    sql = <<-SQL
+            SELECT *
+            FROM pokemon
+            WHERE id = ?
+        SQL
+        
+        x = db.execute(sql, id).map do |row|
+            self.new_from_db(row, db)
+        end.first
   end   
 
 
